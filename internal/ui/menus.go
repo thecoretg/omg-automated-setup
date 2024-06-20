0ccbc1a83a06ae877ec3bbd3a79af9838772eb31
tctgdanny <154454046+tctgdanny@users.noreply.github.com>
2024-06-20 07:57:26 -0500

    fix to user menu - confirm menu output

--- internal/ui/menus.go
+++ internal/ui/menus.go
@@ -26,14 +26,18 @@ func RunSetupTypeMenu(sv *shared.SetupVars) error {
 }
 
 func RunUserMenu(sv *shared.SetupVars) error {
-	form := huh.NewForm(
+	userForm := huh.NewForm(
 		UserRoleMenu(sv),
 		DeleteSpareMenu(sv),
-		UserConfirmMenu(sv),
 	)
 
-	if err := form.WithTheme(huh.ThemeBase()).Run(); err != nil {
+	if err := userForm.WithTheme(huh.ThemeBase()).Run(); err != nil {
 		return fmt.Errorf("error with user setup form: %v", err)
+	}
+
+	confirmForm := UserConfirmMenu(sv)
+	if err := confirmForm.WithTheme(huh.ThemeBase()).Run(); err != nil {
+		return fmt.Errorf("error with user confirm form: %v", err)
 	}
 
 	return nil
@@ -65,16 +69,18 @@ func DeleteSpareMenu(sv *shared.SetupVars) *huh.Group {
 	)
 }
 
-func UserConfirmMenu(sv *shared.SetupVars) *huh.Group {
+func UserConfirmMenu(sv *shared.SetupVars) *huh.Form {
 	confirmMsg := fmt.Sprintf("Full Name: %s\nUsername: %s\nRole: %s\n", sv.FullName, sv.Username, sv.UserRole)
-	return huh.NewGroup(
-		huh.NewSelect[bool]().
-			Title("Is this information correct?").
-			Description(confirmMsg).
-			Options(
-				huh.NewOption("Yes (Send Command)", true).Selected(true),
-				huh.NewOption("No (Exit)", false),
-			).
-			Value(&sv.Confirm),
+	return huh.NewForm(
+		huh.NewGroup(
+			huh.NewSelect[bool]().
+				Title("Is this information correct?").
+				Description(confirmMsg).
+				Options(
+					huh.NewOption("Yes (Send Command)", true).Selected(true),
+					huh.NewOption("No (Exit)", false),
+				).
+				Value(&sv.Confirm),
+		),
 	)
 }
